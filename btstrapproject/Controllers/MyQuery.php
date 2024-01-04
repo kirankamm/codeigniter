dffccfac7ca7c12a05466bc5c6b7f736529406b7
kirankamm <118911532+kirankamm@users.noreply.github.com>
2024-01-04 11:24:46 +0530

    Add files via upload

---
+++ btstrapproject/Controllers/MyQuery.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controllers;
+
+use CodeIgniter\Controller;
+
+class MyQuery extends Controller
+
+{
+public function index(){
+
+    echo view('Project/project'); 
+}
+
+
+}
+?>
