bdb9bd7849c1964a8e83279018c8f8cc6cb0f822
kirankamm <118911532+kirankamm@users.noreply.github.com>
2024-01-27 18:01:48 +0530

    Add files via upload

---
+++ ci4app/CRUDci/find/Models/UserModel.php
@@ -0,0 +1,16 @@
+<?php namespace App\Models; 
+
+use CodeIgniter\Model;
+
+class UserModel extends Model{
+
+
+protected $table="users";
+protected $DBGroup="default";
+protected $allowedFields=['username','email','password'];
+protected $useTimestamps=true;
+protected $validationRules=[];
+protected $validationMessages=[];
+
+}
+?>
